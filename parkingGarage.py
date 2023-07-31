d613808bc8f3c393e3ae9a875ce1faf4bfa5f4cb
Faith Mcquade <faffiemcquade@yahoo.com>
2023-07-30 20:47:20 -0700

    leave garage & changes

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,20 +1,20 @@
+import random
 
 class ParkingGarage:
     def __init__(self):
-        self.taken_tickets = []
         self.spaces_taken = []
         self.current_tickets = {}
-        self.spaces_avail = 50
-        self.tickets_avail = 50
+        self.spaces_avail = ["A1", "B2", "C3", "D1", "E2", "F3", "G1", "H2", "I3"]
+        self.tickets_avail = 9
 
 
     def take_ticket(self):
-        if self.spaces_avail > 0:
-            print(f"Welcome to the Parking Garage, there are {self.spaces_avail} spaces available, please take your ticket.")
-            self.spaces_taken.append(self.spaces_avail)
-            self.taken_tickets.append(self.tickets_avail)
+        if self.tickets_avail > 0:
+            print(f"Welcome to the Parking Garage, there are {self.tickets_avail} tickets available, please take one.")
+            self.assign_space = random.choice(self.spaces_avail)
+            self.spaces_avail.remove(self.assign_space)
+            self.spaces_taken.append(self.assign_space)
             self.current_tickets.update({"Paid": False})
-            self.spaces_avail -= 1 
             self.tickets_avail -= 1
 
         else:
@@ -32,29 +32,43 @@ class ParkingGarage:
             self.payment = int(input("Please insert your payment: (Bills accepted: $5, $10, $20, $50, $100)\n"))
             
             if self.payment == self.cust_total:
-                print("Ticket has been paid. Gate opening. Have a wonderful day :)")
+                print("Ticket has been paid. You have 15 minutes to leave")
                 self.current_tickets.update({"Paid": True})
-                break
-
-
+                paid_in_full = True
+                self.leave_garage()
+                
             elif self.payment > self.cust_total:
-                print("Please take your change. Have a wonderful day :)")
+                print("Please take your change.")
                 self.current_tickets.update({"Paid": True})
-                break
-
+                paid_in_full = True
+                self.leave_garage()
+                
             elif self.payment < self.cust_total:
                 print("Not enough money! You can't leave until you pay me.")
                 
-                while True:
-                    self.payment_addl = int(input(f"${self.cust_total - self.payment} left due. (Bills accepted: $5, $10, $20, $50, $100)\n"))
-                    if (self.payment_addl + self.payment) == self.cust_total:
-                        print("Ticket has been paid. Gate opening. Have a wonderful day :)")
-                        self.current_tickets.update({"Paid": True})
-                        paid_in_full = True
-                        break
-                    else:
-                        self.payment += self.payment_addl
-                        continue
+                
+                self.payment_addl = int(input(f"${self.cust_total - self.payment} left due. (Bills accepted: $5, $10, $20, $50, $100)\n"))
+                if (self.payment_addl + self.payment) == self.cust_total:
+                    print("Ticket has been paid. You have 15 minutes to leave.")
+                    self.current_tickets.update({"Paid": True})
+                    paid_in_full = True
+                    self.leave_garage()
+                else:
+                    self.payment += self.payment_addl
+    
+    def leave_garage(self):
+        if True in self.current_tickets.values():
+            print("Thank you, have a wonderful day! :)")
+            self.tickets_avail += 1
+            self.spaces_taken.remove(self.assign_space)
+            self.spaces_avail.append(self.assign_space)
+            print(self.spaces_taken)
+        elif False in self.current_tickets.values():
+            print("Please return to pay.")
+        return
+
+
+        
 
 
     
